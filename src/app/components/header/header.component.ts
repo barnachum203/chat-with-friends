e01e271e37382263b99037b568138bef0e7f414d
barnachum203 <barnachum203@gmail.com>
2021-08-02 16:37:22 +0300

    Adjusting login and logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public IsLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService) {
+    //Initialze the Observable:
+    this.IsLoggedIn$ = authService.isLoggedIn(); 
+  }
 
   ngOnInit(): void {
   }
 
+  public logInWithGoogle(): void {
+    this.authService.singInWithGoogle();
+  }
+
+  public singOut(): void {
+    this.authService.singOut();
+  }
 }
